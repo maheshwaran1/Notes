980cf3ba43a8757807ec0d2f41ead7b013a47a58
maheshwaran <maheshwaran143m@gmail.com>
2022-02-18 20:07:24 +0530

    Fist Commit

--- Notes/Controllers/CategoryViewController.swift
+++ Notes/Controllers/CategoryViewController.swift
@@ -119,35 +119,3 @@ class CategoryViewController: UITableViewController {
     
     
 }
-
-/*
-class CategoryTableViewController: UITableViewController {
-
-    var categories = [Category]()
-    
-    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-    }
-
-    //MARK: - TableView Datasource Methods
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return categories.count
-    }
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell",for: indexPath)
-        cell.textLabel?.text = categories[indexPath.row].name
-        return cell
-    }
-    
-    //MARK: - Add New Categories
-    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
-        
-    }
-    
-    //MARK: - TableView Datasource Methods
-}
-
-*/
